3b73ce99cc1af8a872d141967bfd69f89d8e84b1
venky <venky@mail.com>
2023-01-11 20:45:28 +0530

    second commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+print("Hello venky")
+print("Hello venky")
+print("Hello venky")
+print("Hello venky")
+
+print("Hello venky")
+print("Hello venky")
+print("Hello venky")
+print("Hello venky")
+print("Hello venky")
